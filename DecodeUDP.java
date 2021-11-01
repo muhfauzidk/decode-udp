052d176df2912ff04b71925e60fbffafd9a58192
muhfauzidk <86062100+muhfauzidk@users.noreply.github.com>
2021-11-01 08:31:03 +0700

    Update DecodeUDP.java

--- DecodeUDP.java
+++ DecodeUDP.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package decode.udp;
 
 import java.lang.*;
@@ -12,10 +8,6 @@ import java.util.Scanner;
  * @author UZI
  */
 public class DecodeUDP {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Input UDP header in hexadecimal: ");
